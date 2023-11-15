c1262bf264a7adcd7d8f13e9d6a86bde37907605
fcardenas886 <alek_886@hotmail.com>
2023-11-15 02:13:12 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(318, 136)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(109, 42)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Generar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(189, 76)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "900"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(106, 79)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Rango Inicio"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(410, 82)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Maximo "
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(490, 76)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(340, 22)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(114, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Generador de Rut"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(327, 255)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,56 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim inicio As String
+        inicio = "01111"
+        Dim i As Integer
+        i = CInt(TextBox1.Text)
+        ' TextBox1.Text = inicio + i.ToString()
+
+
+        For x = i To (i + CInt(TextBox2.Text))
+            Dim st As String
+            st = inicio + x.ToString()
+            TextBox3.Text = inicio + x.ToString()
+            escribir(st)
+        Next
+
+    End Sub
+
+    Function CalcularDigitoVerificador(rutSinVerificador As String) As Char
+        ' Calcular el dígito verificador
+        Dim m As Integer = 2
+        Dim s As Integer = 0
+
+        For Each d As Char In StrReverse(rutSinVerificador)
+            s += Integer.Parse(d.ToString()) * m
+            m = If(m = 7, 2, m + 1)
+        Next
+
+        Dim digitoVerificadorCalculado As Integer = (11 - s Mod 11) Mod 11
+
+        ' Convertir el dígito verificador a Char
+        If digitoVerificadorCalculado = 10 Then
+            Return "K"c
+        Else
+            Return Char.Parse(digitoVerificadorCalculado.ToString())
+        End If
+    End Function
+
+    Sub escribir(ByVal s As String)
+
+        ' Ruta del archivo de texto
+        Dim filePath As String = "C:\x\txt.txt"
+
+        ' Contenido que deseas escribir en el archivo
+        Dim content As String = "Hola, esto es un ejemplo de escritura en un archivo de texto."
+
+        ' Utiliza Using para asegurarte de que el StreamWriter se cierre correctamente al finalizar
+        Using writer As New StreamWriter(filePath, True) ' El segundo parámetro (True) indica que se añadirá al archivo si ya existe
+            writer.WriteLine(s)
+        End Using
+
+    End Sub
+End Class
